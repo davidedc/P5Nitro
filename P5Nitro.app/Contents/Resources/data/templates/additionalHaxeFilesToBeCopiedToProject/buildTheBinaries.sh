860f512a1628f7dcb9a61b7c7a7aef66f1dda9cb
Davide Della Casa <davidedc@gmail.com>
2011-08-02 23:34:20 +0100

    recompiled the mac app

--- P5Nitro.app/Contents/Resources/data/templates/additionalHaxeFilesToBeCopiedToProject/buildTheBinaries.sh
+++ P5Nitro.app/Contents/Resources/data/templates/additionalHaxeFilesToBeCopiedToProject/buildTheBinaries.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
-haxelib > testToSeeIfHaxelibIsInstalled.txt
+which haxelib > testToSeeIfHaxelibIsInstalled.txt
 
 if [ -s testToSeeIfHaxelibIsInstalled.txt ]
   then
     echo "haxelib is already installed"
     export DYLD_LIBRARY_PATH=:/usr/lib/haxe/lib/hxcpp/2,07,0/bin/Mac:/usr/lib/haxe/lib/nme/2,0,1/ndll/Mac:.
-    #export HOME=/Users/davidedellacasa
-    #export LOGNAME=davidedellacasa
     export NEKO_INSTALL_PATH=/usr/lib/neko
-    #export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/git/bin:/usr/X11/bin
-fi    
-    
-
-if [ ! -s testToSeeIfHaxelibIsInstalled.txt ]
-  then
-    echo "haxelib has been installed as portable"
-    export HAXEPATH=$HOME/Downloads/P5Nitro/ToolsAndSources/HaxeNekoAndHaxelib/haxe-2.07-osx/
-    export NEKOPATH=$HOME/Downloads/P5Nitro/ToolsAndSources/HaxeNekoAndHaxelib/neko-1.8.1-osx/
+  else
+	echo "haxelib has been installed as portable"
+    export HAXEPATH=$PWD/../../../ToolsAndSources/HaxeNekoAndHaxelib/haxe-2.07-osx/
+    export NEKOPATH=$PWD/../../../ToolsAndSources/HaxeNekoAndHaxelib/neko-1.8.1-osx/
     export PATH=$HAXEPATH:$NEKOPATH:$PATH
-    export DYLD_LIBRARY_PATH=$HOME/Downloads/P5Nitro/ToolsAndSources/HaxeNekoAndHaxelib/neko-1.8.1-osx/
-    #haxelib setup $HOME/Downloads/P5Nitro/ToolsAndSources/HaxeNekoAndHaxelib/
-    #haxelib install hxcpp
-    #haxelib install nme
-    export HXCPP=$HOME/Downloads/P5Nitro/ToolsAndSources/HaxeNekoAndHaxelib/hxcpp/
-    export HAXE_LIBRARY_PATH=$HOME/Downloads/P5Nitro/ToolsAndSources/HaxeNekoAndHaxelib/haxe-2.07-osx/std/
-    export LD_LIBRARY_PATH=$HOME/Downloads/P5Nitro/ToolsAndSources/HaxeNekoAndHaxelib/hxcpp/2,07,0/bin/Mac/
-    export LD_LIBRARY_PATH=$HOME/Downloads/P5Nitro/ToolsAndSources/HaxeNekoAndHaxelib/nme/2,0,1/ndll/Mac/:$LD_LIBRARY_PATH
+    export DYLD_LIBRARY_PATH=$PWD/../../../ToolsAndSources/HaxeNekoAndHaxelib/neko-1.8.1-osx/
+    export HXCPP=$PWD/../../../ToolsAndSources/HaxeNekoAndHaxelib/hxcpp/
+    export HAXE_LIBRARY_PATH=$PWD/../../../ToolsAndSources/HaxeNekoAndHaxelib/haxe-2.07-osx/std/
+    export LD_LIBRARY_PATH=$PWD/../../../ToolsAndSources/HaxeNekoAndHaxelib/hxcpp/2,07,0/bin/Mac/
+    export LD_LIBRARY_PATH=$PWD/../../../ToolsAndSources/HaxeNekoAndHaxelib/nme/2,0,1/ndll/Mac/:$LD_LIBRARY_PATH
 fi
 
 which gcc > testToSeeIfGccIsInstalled.txt
@@ -34,10 +24,7 @@ if [ -s testToSeeIfGccIsInstalled.txt ]
   then
 	echo "gcc is installed"
 	haxelib run nme build P5NitroSketch.nmml cpp
-fi
-
-if [ ! -s testToSeeIfGccIsInstalled.txt ]
-  then
+  else
 	echo "gcc is not installed"
 	haxe -main Main -neko P5NitroSketch.n -lib nme -lib hxcpp --remap flash:nme
 fi
