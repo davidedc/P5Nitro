6292ca9cba3be39054560e98fbecd35ed17e5578
Davide Della Casa <davidedc@gmail.com>
2011-08-13 21:56:00 +0100

    small tweaks to the linux installer (now removes unneeded osx stuff), plus the gitclient directory has been deleted

--- ToolsAndSources/Installer/P5NitroInstallerLinux.sh
+++ ToolsAndSources/Installer/P5NitroInstallerLinux.sh
@@ -3,6 +3,8 @@ wget -O P5NitroCheckout.zip https://github.com/davidedc/P5Nitro/zipball/master
 unzip P5NitroCheckout.zip
 rm P5NitroCheckout.zip
 mv davidedc-P5Nitro-* P5Nitro
+rm -rd $PWD/P5Nitro/P5Nitro.app
+rm -rd $PWD/P5Nitro/ToolsAndSources/P5Nitro/data/cachedBin/cpp
 
 which haxelib > testToSeeIfHaxelibIsInstalled.txt
 
@@ -12,13 +14,15 @@ if [ -s testToSeeIfHaxelibIsInstalled.txt ]
   else
     echo "haxelib has not been installed"
     mkdir $PWD/P5Nitro/ToolsAndSources/HaxeNekoAndHaxelib
+
     wget -O neko.tgz http://nekovm.org/_media/neko-1.8.1-linux.tar.gz
-    wget -O haxe.tgz http://haxe.org/file/haxe-2.07-linux.tar.gz
-    #curl http://processing.googlecode.com/files/processing-1.5.1-linux.zip > processing.zip
     tar -zxvf neko.tgz -C $PWD/P5Nitro/ToolsAndSources/HaxeNekoAndHaxelib/
+    rm neko.tgz
+
     tar -zxvf haxe.tgz -C $PWD/P5Nitro/ToolsAndSources/HaxeNekoAndHaxelib/
-    #unzip processing.zip
-    rm haxe.tgz neko.tgz
+    wget -O haxe.tgz http://haxe.org/file/haxe-2.07-linux.tar.gz
+    rm haxe.tgz
+
     export HAXEPATH=$PWD/P5Nitro/ToolsAndSources/HaxeNekoAndHaxelib/haxe-2.07-linux/
     export NEKOPATH=$PWD/P5Nitro/ToolsAndSources/HaxeNekoAndHaxelib/neko-1.8.1-linux/
     export PATH=$HAXEPATH:$NEKOPATH:$PATH
